50e6adb9016a01e7d1ea43b68225a5a670484810
umutozgenc34 <umutozgenc34@hotmail.com>
2024-12-03 14:57:36 +0300

    Add project files.

---
+++ ExcelCreateApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExcelCreateApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
