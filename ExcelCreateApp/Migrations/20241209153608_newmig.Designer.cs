ee470b030f35482fde3fa68015ac7f0613cdcd32
umutozgenc34 <umutozgenc34@hotmail.com>
2024-12-09 18:38:31 +0300

    Product controller

--- ExcelCreateApp/Migrations/20241209153608_newmig.Designer.cs
+++ ExcelCreateApp/Migrations/20241209153608_newmig.Designer.cs
@@ -4,6 +4,7 @@ using ExcelCreateApp.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExcelCreateApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241209153608_newmig")]
+    partial class newmig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ExcelCreateApp/Models/UserFile.cs
+++ ExcelCreateApp/Models/UserFile.cs
@@ -13,7 +13,7 @@ public class UserFile
     public int Id { get; set; }
     public string UserId { get; set; }
     public string FileName { get; set; }
-    public string FilePath { get; set; }
+    public string? FilePath { get; set; }
     public DateTime? CreatedDate { get; set; }
     public FileStatus FileStatus { get; set; }
 
